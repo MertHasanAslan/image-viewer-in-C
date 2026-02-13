9d7ff3a4c2b698880477dd9561be0b5568bc4180
MertHasanAslan <merthasanaslan325@gmail.com>
2026-02-14 02:04:31 +0300

    starting to control screen

--- imageViewer.c
+++ imageViewer.c
@@ -4,10 +4,16 @@
 
 int main(){
 
-
-    printf("Hello World! \n");
     SDL_Window* pwindow = SDL_CreateWindow("Image Viewer", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 900, 600, 0);
+
+    SDL_Surface *psurface = SDL_GetWindowSurface(pwindow);
+    
+    SDL_FillRect(psurface, NULL, 0xFF0000FF);
+    SDL_UpdateWindowSurface(pwindow);
     SDL_Delay(3000);
+
+
+
 
     return 0;
 }
