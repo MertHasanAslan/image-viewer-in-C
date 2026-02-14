fe237638e77b103894b15eade15bd9f319dfb160
MertHasanAslan <merthasanaslan325@gmail.com>
2026-02-14 20:46:58 +0300

    last debuggings

--- imageViewer.c
+++ imageViewer.c
@@ -4,6 +4,12 @@
 
 
 int main(){
+    // checking SDL_Init
+    if (SDL_Init(SDL_INIT_VIDEO) != 0){
+        printf("SDL_Init error");
+        return 1;
+    }
+
     // getting filename
     printf("Please enter the filename (only ppm files): ");
     char filename[255];
@@ -14,7 +20,7 @@ int main(){
     filename[strcspn(filename, "\n")] = '\0';
 
     // Opening file
-    FILE* pfile = fopen(filename, "r");
+    FILE* pfile = fopen(filename, "rb");
     if(pfile == NULL){
         perror("File could not open");
         return 1;
@@ -37,34 +43,41 @@ int main(){
 
 
 
+
+
     SDL_Window* pwindow = SDL_CreateWindow("Image Viewer", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height, 0);
 
     SDL_Surface *psurface = SDL_GetWindowSurface(pwindow);
     
-    
+    unsigned char rgb[3];
     Uint8 r, g, b;
     unsigned int tempr, tempg, tempb;
     Uint32 color;
     
     SDL_Rect rect = (SDL_Rect){.x=0, .y=0, .h=1, .w=1};
-    for (int x = 0; x < width; x++){
-        for (int y = 0; y < height; y++){
+    for (int y = 0; y < height; y++){
+        for (int x = 0; x < width; x++){
             rect.x = x;
             rect.y = y;
-            fscanf(pfile, "%u %u %u", &tempr, &tempg, &tempb);
-            if (tempr > 255 || tempg > 255 || tempb > 255) {
-                printf("An error occurred while reading rgb values");
-                return 1;
-            }
-            r = (Uint8)tempr;
-            g = (Uint8)tempg;
-            b = (Uint8)tempb;
-            color = SDL_MapRGB(psurface->format, r, g, b);
+
+            // fscanf(pfile, "%u %u %u", &tempr, &tempg, &tempb);
+            // if (tempr > 255 || tempg > 255 || tempb > 255) {
+            //     printf("An error occurred while reading rgb values");
+            //     return 1;
+            // }
+            // r = (Uint8)tempr;
+            // g = (Uint8)tempg;
+            // b = (Uint8)tempb;
+
+            fread(rgb, 1, 3, pfile);
+            
+
+            color = SDL_MapRGB(psurface->format, rgb[0], rgb[1], rgb[2]);
             SDL_FillRect(psurface, &rect, color);
         }
     }
     SDL_UpdateWindowSurface(pwindow);
-    SDL_Delay(5000);
+    SDL_Delay(3000);
 
 
 
