3a1a3ffdcab1741a244bb7f4ce7e402f09138866
MertHasanAslan <merthasanaslan325@gmail.com>
2026-02-14 16:49:41 +0300

    for loop

--- imageViewer.c
+++ imageViewer.c
@@ -14,16 +14,16 @@ int main(){
     g = 0x82;
     b = 0x80;
     Uint32 color = SDL_MapRGB(psurface->format, r, g, b);
-    SDL_Rect* prect = malloc(sizeof *prect);
-    if (prect!=NULL){
-        *prect = (SDL_Rect){.x=450-100/2, .y=300-100/2, .w=100, .h=100};
+    for (int x = 0; x <= 900; x++){
+        for (int y = 0; y <= 600; y++){
+            SDL_Rect rect = (SDL_Rect){.x=x, .y=y, .h=1, .w=1};
+            SDL_FillRect(psurface, &rect, color);
+        }
     }
-    SDL_FillRect(psurface, prect, color);
     SDL_UpdateWindowSurface(pwindow);
     SDL_Delay(5000);
 
 
 
-    free(prect);
     return 0;
 }
