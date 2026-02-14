afe93109974c65e157d960489008207f74ed8f8c
MertHasanAslan <merthasanaslan325@gmail.com>
2026-02-14 16:43:29 +0300

    starting to make colorful screen

--- imageViewer.c
+++ imageViewer.c
@@ -8,12 +8,22 @@ int main(){
 
     SDL_Surface *psurface = SDL_GetWindowSurface(pwindow);
     
-    SDL_FillRect(psurface, NULL, 0xFF0000FF);
+    
+    Uint8 r, g, b;
+    r = 0xBA;
+    g = 0x82;
+    b = 0x80;
+    Uint32 color = SDL_MapRGB(psurface->format, r, g, b);
+    SDL_Rect* prect = malloc(sizeof *prect);
+    if (prect!=NULL){
+        *prect = (SDL_Rect){.x=450-100/2, .y=300-100/2, .w=100, .h=100};
+    }
+    SDL_FillRect(psurface, prect, color);
     SDL_UpdateWindowSurface(pwindow);
-    SDL_Delay(3000);
+    SDL_Delay(5000);
 
 
 
-
+    free(prect);
     return 0;
 }
