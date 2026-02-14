8182d155886f8e330892bfbd510254014d98d20d
MertHasanAslan <merthasanaslan325@gmail.com>
2026-02-14 20:14:06 +0300

    almots there

--- imageViewer.c
+++ imageViewer.c
@@ -2,21 +2,64 @@
 #include <stdlib.h>
 #include <SDL2/SDL.h>
 
-int main(){
 
-    SDL_Window* pwindow = SDL_CreateWindow("Image Viewer", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 900, 600, 0);
+int main(){
+    // getting filename
+    printf("Please enter the filename (only ppm files): ");
+    char filename[255];
+    if (fgets(filename, sizeof(filename), stdin) == NULL){
+        printf("could not get the filename");
+        return 1;
+    }
+    filename[strcspn(filename, "\n")] = '\0';
+
+    // Opening file
+    FILE* pfile = fopen(filename, "r");
+    if(pfile == NULL){
+        perror("File could not open");
+        return 1;
+    }
+
+    // reading first two lines (specifier and comment)
+    char line[1024];
+    for (int i = 0; i < 2; i++){
+        fgets(line, sizeof(line), pfile);
+        printf("%s", line);
+    }
+    
+    // getting width and height values
+    int width, height;
+    fscanf(pfile, "%d %d\n", &width, &height);
+
+    // reading max color value line
+    fgets(line, sizeof(line), pfile);
+
+
+
+
+    SDL_Window* pwindow = SDL_CreateWindow("Image Viewer", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height, 0);
 
     SDL_Surface *psurface = SDL_GetWindowSurface(pwindow);
     
     
     Uint8 r, g, b;
-    r = 0xBA;
-    g = 0x82;
-    b = 0x80;
-    Uint32 color = SDL_MapRGB(psurface->format, r, g, b);
-    for (int x = 0; x <= 900; x++){
-        for (int y = 0; y <= 600; y++){
-            SDL_Rect rect = (SDL_Rect){.x=x, .y=y, .h=1, .w=1};
+    unsigned int tempr, tempg, tempb;
+    Uint32 color;
+    
+    SDL_Rect rect = (SDL_Rect){.x=0, .y=0, .h=1, .w=1};
+    for (int x = 0; x < width; x++){
+        for (int y = 0; y < height; y++){
+            rect.x = x;
+            rect.y = y;
+            fscanf(pfile, "%u %u %u", &tempr, &tempg, &tempb);
+            if (tempr > 255 || tempg > 255 || tempb > 255) {
+                printf("An error occurred while reading rgb values");
+                return 1;
+            }
+            r = (Uint8)tempr;
+            g = (Uint8)tempg;
+            b = (Uint8)tempb;
+            color = SDL_MapRGB(psurface->format, r, g, b);
             SDL_FillRect(psurface, &rect, color);
         }
     }
